03ad7899221747c785e27746a944576ed949a4d4
David Simpson <davidaniel91@gmail.com>
2017-03-27 15:26:50 +0000

    Created Controllers and views

---
+++ app/controllers/review_controller.rb
@@ -0,0 +1,28 @@
+class ReviewController < ApplicationController
+    def show
+    end
+    
+    def new
+    end
+    
+    def create
+    end
+    
+    def index
+    end
+    
+    def edit
+    end
+    
+    def update
+    end
+    
+    def destroy
+    end
+    
+    
+end
+
+private
+def review_params
+end
