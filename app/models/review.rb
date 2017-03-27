cf64d71aac1746fd0b997eeb48813436dce95a41
David Simpson <davidaniel91@gmail.com>
2017-03-27 16:20:46 +0000

    Add validate rating

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,7 @@
 class Review < ApplicationRecord
     validates :title, presence: true, length: { minimum: 1 }
     validates :text, presence: true, length: { minimum: 1 }
+    validate :rating, prescence: true, length: { minimum:1 }
+    validate :rating, prescence: true, length: { maximum:5 }
 end
+
