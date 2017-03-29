59149fc3f1ed6798abb1b450349d1dff57a26425
David Simpson <davidaniel91@gmail.com>
2017-03-29 15:55:02 +0000

    Welcome Page CSS

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,5 @@ class Review < ApplicationRecord
  validates :title, presence: true, length: { minimum: 5 }
  validates :text, presence: true, length: { minimum: 10 }
  validates :rating,presence: true, numericality: {only_decimal: true}
-
+ validates :rating, :numericality => {:greater_than => 0, :less_than_or_equal_to => 5}
 end
