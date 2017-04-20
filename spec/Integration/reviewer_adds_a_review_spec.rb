5c4833fc4d4e07f5e20a9453b00a0cdbbbcdedeb
David Simpson <davidaniel91@gmail.com>
2017-04-20 17:33:09 +0000

    cleaned up the code changed review CSS

--- spec/Integration/reviewer_adds_a_review_spec.rb
+++ spec/Integration/reviewer_adds_a_review_spec.rb
@@ -1,17 +1,16 @@
-# require 'rails_helper.rb'
-# require 'spec_helper.rb'
-# include Capybara::DSL
+require 'rails_helper.rb'
+require 'spec_helper.rb'
+include Capybara::DSL
 
-# feature "reviewer adds a review" do
-#     scenario "Reviewer successfuly navigates to a restaurant page and adds a review" do
-#     visit restaurants_path
-#     expect(page).to have_content("Listing restaurants")
-#     sleep 2
-#     # page.all('a')[3].click_link
-#     first('.listing > a')
-#     expect(page).to have_field("Commenter")
-#     expect(page).to have_field("Review")
-#     expect(page).to have_field("Rating")
-#     end
+feature "reviewer adds a review" do
+    scenario "Reviewer successfuly navigates to a restaurant page and adds a review" do
+    visit restaurants_path
+    expect(page).to have_content("Listing restaurants")# 
+    #page.all('a')[3].click_link
+    #click_on("first_link")
+    expect(page).to have_field("Commenter")
+    expect(page).to have_field("Review")
+    expect(page).to have_field("Rating")
+    end
     
-# end
+end
