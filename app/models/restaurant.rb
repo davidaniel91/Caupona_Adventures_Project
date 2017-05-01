e2342279bfe3d79a0bda0961533188243b02eed8
David Simpson <davidaniel91@gmail.com>
2017-05-01 18:01:34 +0000

    Changes made to restaurant model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,8 @@
 class Restaurant < ApplicationRecord
 has_many :reviews, dependent: :destroy
 validates :name, presence: true, length: { minimum: 5 }
-validates :name, length: { maximum: 30 }
-validates :location, presence: true, length: {maximum: 50}
+validates :name, length: { maximum: 50 }
+validates :location, presence: true, length: {maximum: 60}
 validates :description, presence: true, length: { minimum: 50}
 validates :description, length: { maximum: 255 }
 end
