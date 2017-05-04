54bfd0fc87082fb91214543fb26e81103d4d43d9
David Simpson <davidaniel91@gmail.com>
2017-05-04 06:26:50 +0000

    Added image upload functionality, David Simpson

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :places
   get 'about/index'
 
   get 'reviews/index'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170410153304) do
+ActiveRecord::Schema.define(version: 20170504044148) do
+
+  create_table "places", force: :cascade do |t|
+    t.string   "name"
+    t.string   "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
